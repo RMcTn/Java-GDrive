0d13ae2cb42e594bee4248c62f3b5b0db39553b6
Reece <rmctiernan1@gmail.com>
2018-01-16 16:08:00 +0000

    Add Update for updating drive files with local ones

---
+++ src/main/java/Update.java
@@ -0,0 +1,47 @@
+import com.google.api.client.http.FileContent;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static java.nio.file.Files.probeContentType;
+
+/*
+   For updating files on the google drive (local files being uploaded to gdrive)
+    */
+public class Update {
+
+    public static FileList getFileListFromDrive(String fileName) {
+        Drive service = GDrive.getDriveService();
+        String query = String.format("name = '%s'", fileName);
+        FileList result = null;
+        try {
+            result = service.files().list().setQ(query).setFields("files(id, name, mimeType, md5Checksum, parents)").execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static void updateFile(String driveFileID, String localFileName) {
+        Drive service = GDrive.getDriveService();
+        String path = GDrive.getDrive_dir() + localFileName;
+        java.io.File localFile = new java.io.File(path);
+        System.out.println(localFile.getAbsolutePath());
+        String mimeType;
+        try {
+            mimeType = probeContentType(localFile.toPath());
+            FileContent mediaContent = new FileContent(mimeType, localFile);
+            File file = new File();
+            file.setName(localFileName);
+            service.files().update(driveFileID, file, mediaContent).execute();
+            System.out.printf("Updated drive file %s (%s) with local file %s\n", file.getName(), driveFileID, localFileName);
+        } catch (IOException e) {
+            System.err.println("Error updating drive file " + localFileName + " (" + driveFileID + ") " + e.getMessage());
+            return;
+        }
+    }
+}
